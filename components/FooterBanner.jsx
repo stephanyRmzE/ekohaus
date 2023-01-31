25f10aca64a13610fed31a1f8d05b5bfe74e1850
Stephany Ramirez <stephany.ramireze@gmail.com>
2023-01-31 13:43:23 -0700

    basics of website created

---
+++ components/FooterBanner.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Link from 'next/link'
+
+
+const FooterBanner = ({footerBanner}) => {
+  return (
+    <div>FooterBanner</div>
+  )
+}
+
+export default FooterBanner
