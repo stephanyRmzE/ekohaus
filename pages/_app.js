1507e3d5b93aef035ede2c3881fc6805c944e179
Stephany Ramirez <stephany.ramireze@gmail.com>
2023-02-09 04:27:16 -0700

    canceled page added it and changes in the schema of sanity

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,14 @@
+import {useEffect} from 'react'
 import '../styles/globals.css'
 import { Layout } from '../components'
 import { StateContext } from '../context/StateContext'
 import { Toaster } from 'react-hot-toast'
 
+
+
 export default function App({ Component, pageProps }) {
+
+
   return (
     <StateContext>
       <Layout>
