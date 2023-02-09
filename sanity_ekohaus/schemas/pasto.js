2d3e2735a18a007664e1f3c540f70cccedd38998
Stephany Ramirez <stephany.ramireze@gmail.com>
2023-02-09 08:31:13 -0700

    mas de dos tipos de productos

---
+++ sanity_ekohaus/schemas/pasto.js
@@ -0,0 +1,49 @@
+export default {
+  name: 'pasto',
+  title: 'Pasto',
+  type: 'document',
+  fields: [
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'array',
+      of: [ {type: 'image'}],
+      options: {
+        hotspot: true,
+      }
+    },
+    {
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'name',
+        maxLength: 90,
+      }
+    },
+    {
+      name: 'price',
+      title: 'Price',
+      type: 'number',
+      validation: (Rule) => Rule.positive(),
+      options: {
+      decimalScale: 2,
+      fixedDecimalScale: true,
+      allowNegative: false,
+      decimalSeparator: ',',
+      thousandSeparator: '.',
+      prefix: '$ ',
+  },
+    },
+    {
+      name: 'details',
+      title: 'Details',
+      type: 'string',
+    },
+  ]
+}
