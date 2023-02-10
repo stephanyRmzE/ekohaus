e9acdc60a7e20a06d534eb048da44293e950527b
Stephany Ramirez <stephany.ramireze@gmail.com>
2023-02-10 02:55:28 -0700

    you can now send an email from the page

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,22 +1,52 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Link from 'next/link'
 import {AiOutlineShopping} from 'react-icons/ai'
-
 import {Cart} from './'
 import {useStateContext} from '../context/StateContext'
+
 
 export const Navbar = () => {
 
   const { showCart, setShowCart, totalQuantities } = useStateContext();
+
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
+
   return (
-    <div className="navbar-container">
+
+    <div className="navbar">
       <p className='logo'>
         <Link href='/'>
           <img src='/assets/ekohaus_logo.png' alt="logo ekohaus" />
         </Link>
       </p>
 
-      
+      <div className="menu-icons" onClick={handleClick}>
+            <i className={click ? 'fas fa-times navbar-more' : 'navbar-more fas fa-bars'}></i>
+          </div>
+
+          <ul className={click ? 'nav-menu active' : 'nav-menu '}>
+
+            <li  >
+              <Link
+              href='/'
+              >Acerca de Nosotros</Link>
+            </li>
+            <li >
+              <Link
+              href='/'
+            >Servicios</Link>
+            </li>
+
+            <li  >
+              <Link
+              href='/'
+            >Contactanos</Link>
+            </li>
+
+          </ul>
+
+
 
       <button type='button' className='cart-icon' onClick={() => setShowCart(true)}>
         <AiOutlineShopping/>
@@ -24,5 +54,7 @@ export const Navbar = () => {
       </button>
       {showCart && <Cart/>}
     </div>
+
+
   )
 }
