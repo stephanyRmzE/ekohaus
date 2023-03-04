b1ab279cf895d0e206ce23493531499173a144fb
Stephany Ramirez <stephany.ramireze@gmail.com>
2023-03-04 02:01:59 -0700

    deploy prueba

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
-
-module.exports = {
+const removeImports = require('next-remove-imports')()
+module.exports = removeImports({
    images: {
 		domains: ['cdn.sanity.io'],
 		loader: 'custom'
 	},
-
-};
+  useFileSystemPublicRoutes: false,
+  webpack(config, options) {
+    return config
+  },
+});
