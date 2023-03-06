bff24a95625c7e002444a531ffff9c4ebdb05e44
Stephany Ramirez <stephany.ramireze@gmail.com>
2023-03-05 23:00:45 -0700

    try build checking pages

--- pages/_app.js
+++ pages/_app.js
@@ -3,16 +3,15 @@ import { Layout } from '../components'
 import { StateContext } from '../context/StateContext'
 import { Toaster } from 'react-hot-toast'
 import '../public/DataPicker.css'
-import { useRouter } from 'next/router'
 
 export default function App({ Component, pageProps }) {
-  const router = useRouter()
+
 
   return (
     <StateContext>
       <Layout>
         <Toaster/>
-        <Component key={router.asPath} {...pageProps} />
+        <Component {...pageProps} />
       </Layout>
     </StateContext>
   )
