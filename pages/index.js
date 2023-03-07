b1636d5b5235b9a7d46e13a6a42473e8dfbd755e
Stephany Ramirez <stephany.ramireze@gmail.com>
2023-03-06 21:28:40 -0700

    sanity database had more elements

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,16 @@
-import React, {useState, useEffect} from 'react'
+import React from 'react'
 import {client} from '../lib/client.js'
-import { Product, HeroBanner, Show, Spinner} from '../components/index'
+import { Product, HeroBanner, Show} from '../components/index'
 import useEmblaCarousel from 'embla-carousel-react'
 
 const OPTIONS = { slidesToScroll: 'auto', containScroll: 'trimSnaps' }
 
 function Home({ murosData,  bannerData, galleryShow } ) {
   const [emblaRef] = useEmblaCarousel(OPTIONS)
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    if (bannerData.length > 0) {
-    setLoading(false)
-    }
-    }, [])
-
-
   return (
     <div>
 
-      {loading ? (
-        <Spinner />
-      ):
       <HeroBanner heroBanner = {bannerData.length && bannerData[0]}/>
-      }
       <div className='products-heading'>
         <h2>Muro Verde Artificial</h2>
         <p>Ideales para usos interiores y exteriores. Pueden utilizarse para proteger y recubrir bardas de concreto</p>
@@ -50,7 +37,6 @@ export async function getStaticProps() {
   // Fetch data from external API
   const murosQuery = '*[_type == "muros"]'
   const murosData = await client.fetch(murosQuery)
-
 
   const bannerQuery = '*[_type == "banner"]'
   const bannerData = await client.fetch(bannerQuery)
